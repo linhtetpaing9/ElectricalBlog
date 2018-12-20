139d2d814069524638364affa6451d4e2a4cea11
Lin Htet Paing <linhtetpaing9@gmail.com>
2018-12-20 10:47:22 +0630

    Modified: Static Embed_link to Dynamic

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -35,7 +35,7 @@
     @endif
     <br>
       <div class="plyr__video-embed" id="player">
-        <iframe src="https://www.youtube.com/embed/zxkV45xc96M?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1" 
+        <iframe src="https://www.youtube.com/embed/{{$video->embed_link}}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1" 
         allowfullscreen allowtransparency allow="autoplay"></iframe>
       </div>
       <div id="app">
